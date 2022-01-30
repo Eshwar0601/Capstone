9485cdcc7a743fb8d6d004e26fc1beae8110ffc7
Eshwar0601 <852eshwar@gmail.com>
2022-01-30 20:28:06 +0530

    Added CORS Support.

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ mongoose.connect(DB_URL, { useNewUrlParser: true }, () => {
 });
 
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use("/api/user", authRouter);
 app.use("/api/posts", postRoute);
 
--- routes/auth.js
+++ routes/auth.js
@@ -36,7 +36,7 @@ router.post("/login", async (req, res) => {
 
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass) {
-        return res.status(400).send("Invalid credentials.");
+        return res.status(400).send({ error: "Invalid credentials." });
     }
 
     const token = jwt.sign({ _id: user._id }, "PotatoInYourASS");
